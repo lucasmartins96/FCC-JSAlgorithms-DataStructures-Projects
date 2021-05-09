dc580aa9323868b414fb570dd37d867bcb996905
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-05-09 10:17:45 -0300

    Adicionando o teste do tipo de retorno

--- tests/telephoneNumberValidator.test.js
+++ tests/telephoneNumberValidator.test.js
@@ -1,1 +1,11 @@
 const telephoneCheck = require('../telephone-number-validator/telephoneNumberValidator');
+
+describe('Testing telephoneCheck function', () => {
+  describe('Testing function return', () => {
+    test('Checks if the returned value is boolean', () => {
+      expect(telephoneCheck('555-555-5555')).toBe(true);
+      expect(telephoneCheck('555555-5555')).toBe(false);
+      expect(telephoneCheck('555-555-5555')).not.toBe('555-555-5555');
+    });
+  });
+});
