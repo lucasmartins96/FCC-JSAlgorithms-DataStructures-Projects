c054927ac4d103f56f654dfdf014d2ed35e1034c
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-05-09 10:36:59 -0300

    Adicionando testes que devem retornar false

--- tests/telephoneNumberValidator.test.js
+++ tests/telephoneNumberValidator.test.js
@@ -43,4 +43,83 @@ describe('Testing telephoneCheck function', () => {
       expect(telephoneCheck('1 456 789 4444')).toBe(resultTest);
     });
   });
+
+  describe('Testing cases that must return false', () => {
+    const resultTest = false;
+    test('Testing with the string "555-5555"', () => {
+      expect(telephoneCheck('555-5555')).toBe(resultTest);
+    });
+
+    test('Testing with the string "5555555"', () => {
+      expect(telephoneCheck('5555555')).toBe(resultTest);
+    });
+
+    test('Testing with the string "1 555)555-5555"', () => {
+      expect(telephoneCheck('1 555)555-5555')).toBe(resultTest);
+    });
+
+    test('Testing with the string "123**&!!asdf#"', () => {
+      expect(telephoneCheck('123**&!!asdf#')).toBe(resultTest);
+    });
+
+    test('Testing with the string "55555555"', () => {
+      expect(telephoneCheck('55555555')).toBe(resultTest);
+    });
+
+    test('Testing with the string "(6054756961)"', () => {
+      expect(telephoneCheck('(6054756961)')).toBe(resultTest);
+    });
+
+    test('Testing with the string "2 (757) 622-7382"', () => {
+      expect(telephoneCheck('2 (757) 622-7382')).toBe(resultTest);
+    });
+
+    test('Testing with the string "0 (757) 622-7382"', () => {
+      expect(telephoneCheck('0 (757) 622-7382')).toBe(resultTest);
+    });
+
+    test('Testing with the string "-1 (757) 622-7382"', () => {
+      expect(telephoneCheck('-1 (757) 622-7382')).toBe(resultTest);
+    });
+
+    test('Testing with the string "2 757 622-7382"', () => {
+      expect(telephoneCheck('2 757 622-7382')).toBe(resultTest);
+    });
+
+    test('Testing with the string "10 (757) 622-7382"', () => {
+      expect(telephoneCheck('10 (757) 622-7382')).toBe(resultTest);
+    });
+
+    test('Testing with the string "27576227382"', () => {
+      expect(telephoneCheck('27576227382')).toBe(resultTest);
+    });
+
+    test('Testing with the string "(275)76227382"', () => {
+      expect(telephoneCheck('(275)76227382')).toBe(resultTest);
+    });
+
+    test('Testing with the string "2(757)6227382"', () => {
+      expect(telephoneCheck('2(757)6227382')).toBe(resultTest);
+    });
+
+    test('Testing with the string "2(757)622-7382"', () => {
+      expect(telephoneCheck('2(757)622-7382')).toBe(resultTest);
+    });
+
+    test('Testing with the string "555)-555-5555"', () => {
+      expect(telephoneCheck('555)-555-5555')).toBe(resultTest);
+    });
+
+    test('Testing with the string "(555-555-5555"', () => {
+      expect(telephoneCheck('(555-555-5555')).toBe(resultTest);
+    });
+
+    test('Testing with the string "(555)5(55?)-5555"', () => {
+      expect(telephoneCheck('(555)5(55?)-5555')).toBe(resultTest);
+    });
+
+    test('Testing with the string "55 55-55-555-5"', () => {
+      expect(telephoneCheck('55 55-55-555-5')).toBe(resultTest);
+    });
+  });
 });
