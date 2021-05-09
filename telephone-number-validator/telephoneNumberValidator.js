dabe17fdbc0058f14ff0777ee1410b44f385b5ee
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-05-09 15:15:08 -0300

    Adicionando a minha resoluçao do algoritmo

--- telephone-number-validator/telephoneNumberValidator.js
+++ telephone-number-validator/telephoneNumberValidator.js
@@ -1,6 +1,6 @@
 function telephoneCheck(str) {
-  return true;
+  const telephoneRegex = /^([1]?)\s?(\(\d{3}\)|\d{3})\s?\-?(\d{3})\s?\-?(\d{4})$/g;
+  return telephoneRegex.test(str);
 }
 
-telephoneCheck("555-555-5555");
 module.exports = telephoneCheck;
