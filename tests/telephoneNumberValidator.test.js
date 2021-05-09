28b6e0035b58557ff2889ef03d3d843fe66dff82
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-05-09 10:25:40 -0300

    Adicionando testes que devem retornar true

--- tests/telephoneNumberValidator.test.js
+++ tests/telephoneNumberValidator.test.js
@@ -8,4 +8,39 @@ describe('Testing telephoneCheck function', () => {
       expect(telephoneCheck('555-555-5555')).not.toBe('555-555-5555');
     });
   });
+
+  describe('Testing cases that must return true', () => {
+    const resultTest = true;
+    test('Testing with the string "1 555-555-5555"', () => {
+      expect(telephoneCheck('1 555-555-5555')).toBe(resultTest);
+    });
+
+    test('Testing with the string "1 (555) 555-5555"', () => {
+      expect(telephoneCheck('1 (555) 555-5555')).toBe(resultTest);
+    });
+
+    test('Testing with the string "5555555555"', () => {
+      expect(telephoneCheck('5555555555')).toBe(resultTest);
+    });
+
+    test('Testing with the string "555-555-5555"', () => {
+      expect(telephoneCheck('555-555-5555')).toBe(resultTest);
+    });
+
+    test('Testing with the string "(555)555-5555"', () => {
+      expect(telephoneCheck('(555)555-5555')).toBe(resultTest);
+    });
+
+    test('Testing with the string "1(555)555-5555"', () => {
+      expect(telephoneCheck('1(555)555-5555')).toBe(resultTest);
+    });
+
+    test('Testing with the string "1 555 555 5555"', () => {
+      expect(telephoneCheck('1 555 555 5555')).toBe(resultTest);
+    });
+
+    test('Testing with the string "1 456 789 4444"', () => {
+      expect(telephoneCheck('1 456 789 4444')).toBe(resultTest);
+    });
+  });
 });
