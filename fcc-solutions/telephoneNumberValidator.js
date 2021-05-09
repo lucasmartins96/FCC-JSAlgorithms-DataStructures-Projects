af7ba2b8a0c8a02ce848b68543565d5baffa8902
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-05-09 15:23:34 -0300

    Adicionando o gabarito

---
+++ fcc-solutions/telephoneNumberValidator.js
@@ -0,0 +1,14 @@
+/*
+Source: <https://forum.freecodecamp.org/t/freecodecamp-challenge-guide-telephone-number-validator/16090>
+*/
+//Solution 1
+function telephoneCheck(str) {
+  var regex = /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/;
+  return regex.test(str);
+}
+
+//Solution 2
+function telephoneCheck(str) {
+  var re = /^([+]?1[\s]?)?((?:[(](?:[2-9]1[02-9]|[2-9][02-8][0-9])[)][\s]?)|(?:(?:[2-9]1[02-9]|[2-9][02-8][0-9])[\s.-]?)){1}([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2}[\s.-]?){1}([0-9]{4}){1}$/;
+  return re.test(str);
+}
